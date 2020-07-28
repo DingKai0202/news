c9d7aec8562dcf69b0c9fc19ed19778a5ae41886
丁凯 <w_dingkai@belink.com>
2020-07-29 00:17:09 +0800

    mvc

--- my_demo/routes/users.js
+++ my_demo/routes/users.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+const UserService = require('../services/user_service');
+const { users } = require('../models/in_memo/user');
+
+router.get('/', (req, res) => {
+  const user = UserService.getAllUsers();
+  res.locals.users = users;
+  res.render('user');
+})
+
+router.post('/', (req, res) => {
+  const { firstName, lastName, age } = req.body
+  const u = UserService.addNewUser(firstName, lastName, age);
+  res.json(u);
+})
+
+router.get('/:userId', (req, res) => {
+  console.log(req.params.userId);
+  const user = UserService.getUserById(Number(req.params.userId));
+  res.locals.user = user;
+  res.render('user');
+})
+
+router.post('/:userId/subscription', (req, res, next) => {
+  try{
+    const sub = UserService.createSubscription(Number(req.params.userId), req.body.url);
+    res.json(sub);
+  } catch (e) {
+    next(e);
+  }
+})
 
 module.exports = router;
